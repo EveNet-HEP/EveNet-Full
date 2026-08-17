06c3181d9a0f7df3e30edbb1c355b6fec8b5a3ad
Yulei Zhang <yulei.zhang@cern.ch>
2026-08-17 20:47:15 +0200

    add new neutrino implementation

--- scripts/engine.py
+++ scripts/engine.py
@@ -809,7 +809,7 @@ class EveNetEngine(L.LightningModule):
                 num_steps=self.neutrino_diffusion_steps,
                 use_tqdm=False,
                 process_name=f"Neutrino",
-                remove_padding=True,
+                remove_padding=False,
             )
 
             for i in range(data_shape[-1]):
